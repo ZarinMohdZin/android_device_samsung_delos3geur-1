26149c07071a34ebe469f4585ce6e2d2c7daf939
David Rouleau <Shabbypenguin@gmail.com>
2013-09-19 01:19:04 -0400

    mkvendor.sh: Initial commit of delos3geur

---
+++ device_delos3geur.mk
@@ -0,0 +1,24 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+$(call inherit-product-if-exists, vendor/samsung/delos3geur/delos3geur-vendor.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/samsung/delos3geur/overlay
+
+LOCAL_PATH := device/samsung/delos3geur
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+$(call inherit-product, build/target/product/full.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := full_delos3geur
+PRODUCT_DEVICE := delos3geur
